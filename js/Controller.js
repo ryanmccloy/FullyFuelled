2dacf2f19c1077d64a5baa693cc45137df141e20
Ryan McCloy <ryannmccloy@gmail.com>
2023-07-26 17:46:01 +0100

    refactured controller.js

--- js/Controller.js
+++ js/Controller.js
@@ -9,39 +9,50 @@ import MAPS_API_KEY from "./Config.js";
 const heroView = new HeroView();
 const mapView = new MapView();
 
-// Handlers
-const showSignUpModelHandler = function () {
-  heroView.SignUpModel.classList.remove("hidden");
-  heroView.Overlay.classList.remove("hidden");
-};
-
-const showLogInModelHandler = function () {
-  heroView.LogInModel.classList.remove("hidden");
-  heroView.Overlay.classList.remove("hidden");
-};
-
-const showMapBoxHandler = function (event) {
-  event.preventDefault();
-  mapView.MapBox.classList.remove("hidden");
-  document.body.classList.add("map-box-visible");
-};
-
-const resetHeroSectionHandler = function () {
-  mapView.MapBox.classList.add("hidden");
-  document.body.classList.remove("map-box-visible");
-};
-
 // Controller Class
 
-export default class Controller {}
+export default class Controller {
+  constructor() {
+    // Handlers
+    this.showSignUpModelHandler = this.showSignUpModelHandler.bind(this);
+    this.showLogInModelHandler = this.showLogInModelHandler.bind(this);
+    this.showMapBoxHandler = this.showMapBoxHandler.bind(this);
+    this.resetHeroSectionHandler = this.resetHeroSectionHandler.bind(this);
+  }
 
-/////////////// Hero
+  // Event Handlers
+  showSignUpModelHandler = function () {
+    heroView.SignUpModel.classList.remove("hidden");
+    heroView.Overlay.classList.remove("hidden");
+  };
 
-heroView.showSignUpModel(showSignUpModelHandler);
-heroView.showLogInModel(showLogInModelHandler);
-heroView.resetHeroSection(resetHeroSectionHandler);
+  showLogInModelHandler = function () {
+    heroView.LogInModel.classList.remove("hidden");
+    heroView.Overlay.classList.remove("hidden");
+  };
+
+  showMapBoxHandler = function (event) {
+    event.preventDefault();
+    mapView.MapBox.classList.remove("hidden");
+    document.body.classList.add("map-box-visible");
+  };
+
+  resetHeroSectionHandler = function () {
+    mapView.MapBox.classList.add("hidden");
+    document.body.classList.remove("map-box-visible");
+  };
+
+  // Initialize the event listeners
+  initializeEventListeners() {
+    heroView.showSignUpModel(this.showSignUpModelHandler);
+    heroView.showLogInModel(this.showLogInModelHandler);
+    heroView.resetHeroSection(this.resetHeroSectionHandler);
+    mapView.showMapBox(this.showMapBoxHandler);
+  }
+}
+
+//////////////
+
+const controller = new Controller();
+controller.initializeEventListeners();
 heroView.initializeEventListeners();
-
-////////////// Map
-
-mapView.showMapBox(showMapBoxHandler);
